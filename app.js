36999c1ef79b06c0fe7fcf99892b49ca1b25601b
peter <leepeter019@gmail.com>
2021-09-04 16:48:31 -0700

    Added multiple origins to allow access during development.

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ app.use(cors())
 const server = require('http').createServer(app);
 const io = require('socket.io')(server, {
   cors: {
-    origin: "https://www.chatapp.site",
+    origin: ["https://www.chatapp.site", "http://localhost:3000"],
     methods: ["GET", "POST"],
     allowedHeaders: ["my-custom-header"],
     credentials: true
